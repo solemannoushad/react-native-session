1b5e474537b8139c6d6cc9c54125c4bbfa7ebf77
solemannoushad <solemannoushad31@gmail.com>
2024-04-09 15:55:26 +0500

    folder-structure

---
+++ task/components/CustomFlatlist.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function CustomFlatlist() {
+  return (
+    <View>
+      <Text>CustomFlatlist</Text>
+    </View>
+  )
+}
